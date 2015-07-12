e1a27b53dca237f5d0ba8407ff343d6356b4ff03
Federico Capaldo <federico.capaldo@yahoo.co.uk>
2015-07-12 18:02:41 +0100

    commit before further branching for manual controller renaming

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,10 @@
 class UserController < ApplicationController
 
+  def show 
+  	@user = User.find(params[:id]) #this bit looks up for the right user when prompt to show info about the self user
+  end
+
+
   def new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'siamonoi' => 'static_pages#siamonoi'
   get 'thirdpage' => 'static_pages#thirdpage'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
