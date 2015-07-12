a90095e8fc65ce3aba945451fb7a72a41db3ab15
Federico Capaldo <federico.capaldo@yahoo.co.uk>
2015-07-12 18:18:55 +0100

    renaming successfully executed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
 
   def show 
   	@user = User.find(params[:id]) #this bit looks up for the right user when prompt to show info about the self user
